b42f6fd03d1acff3329037430b5f0a74de6353f8
iamzr <iamziarehman@gmail.com>
2022-01-04 13:25:15 +0000

    End of section 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template: ` <div><h1>Hi</h1></div> `,
+  template: `
+    <div>
+      <h1>Hi</h1>
+      <pm-products></pm-products>
+    </div>
+  `,
 })
 export class AppComponent {
   pageTitle: string = 'ACME Produce Manager';
