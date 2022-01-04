283d1569651146fe7336f353b16f17baa92ea5da
Zia Rehman <iamziarehman@gmail.com>
2022-01-04 12:20:44 +0000

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  template: ` <div><h1>Hi</h1></div> `,
+})
+export class AppComponent {
+  pageTitle: string = 'ACME Produce Manager';
+}
